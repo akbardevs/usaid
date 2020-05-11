9f1b3de94d06b752741bb7130f8f1cc6560a69fe
Nur Insan Akbar <akbardevs02@gmail.com>
2020-05-11 22:13:00 +0800

    belum

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 500
+                    Laravel 5000
                 </div>
 
                 <div class="links">
