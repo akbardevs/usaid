f80e7afa08693eaf2f45e698fe165db03f9a3461
akbardevs <65167903+akbardevs@users.noreply.github.com>
2020-05-11 21:57:53 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 50
+                    Laravel 500
                 </div>
 
                 <div class="links">
