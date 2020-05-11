61d4d14daa69efd5eeaf234c84a0046d2bb81210
Nur Insan Akbar <akbardevs02@gmail.com>
2020-05-11 23:07:41 +0800

    akbarasd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Riswan
+                    akbar02
                 </div>
 
                 <div class="links">
