f32d55923f02450948d1fb982991de4c4ff9c650
akbardevs <65167903+akbardevs@users.noreply.github.com>
2020-05-11 21:04:57 +0800

    km

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 5
+                    Laravel 50
                 </div>
 
                 <div class="links">
